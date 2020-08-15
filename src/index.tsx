d9d1af6e7b7880748160593d47a6fa6abada91ce
Victor Quiroz <victorhqc@gmail.com>
2020-08-15 16:54:22 +0200

    feat: tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Root from './components/Root';
-import App from './components/App';
+// import Root from './components/Root';
+// import App from './components/App';
 
-import './styles/index.css';
+import './styles/tailwind.css';
 
 const JSApp = () => (
-  <Root>
-    <App />
-  </Root>
+  <>
+    <h1>Hello world 🐕</h1>
+    <p>This doesn't seem to be working properly</p>
+    <img
+      className="rotate"
+      src="https://media.giphy.com/media/etn52ENYVnpxqMaXiT/giphy.gif"
+      alt="pug in a pool"
+    />
+  </>
 );
 
 ReactDOM.render(<JSApp />, document.getElementById('root'));
